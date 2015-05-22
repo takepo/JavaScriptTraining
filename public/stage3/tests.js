ba13f28cac8f41a3c8c7fd9374ca12737e9a83c8
竹島菜穂子 <takeshima.nahoko@lancers.co.jp>
2015-05-22 15:28:00 +0900

    stage3

--- public/stage3/tests.js
+++ public/stage3/tests.js
@@ -8,9 +8,9 @@ describe('ステージ3（意図した通りに DOM 要素の構造を変更で�
       //
       // ここに以下のコードを記述してください。
       //
-      // var element = document.querySelector('#firebrick');
-      // var ghost = document.querySelector('.firebrick-ghost');
-      // element.removeChild(ghost);
+      var element = document.querySelector('#firebrick');
+      var ghost = document.querySelector('.firebrick-ghost');
+      element.removeChild(ghost);
 
 
       var firebrick = document.getElementById('firebrick');
@@ -22,7 +22,9 @@ describe('ステージ3（意図した通りに DOM 要素の構造を変更で�
     it('2 番の要素からインベーダー要素を除去する', function() {
 
       // ここにコードを記述してください。
-
+      var element = document.querySelector('#chocolate');
+      var ghost = document.querySelector('.chocolate-space-invader');
+      element.removeChild(ghost);
 
       var darkorange = document.getElementById('chocolate');
       expect(darkorange.childNodes.length).to.equal(1);
@@ -33,6 +35,10 @@ describe('ステージ3（意図した通りに DOM 要素の構造を変更で�
     it('3 番の要素の左右の幽霊要素をすべて除去する', function() {
 
       // ここにコードを記述してください。
+      var element = document.querySelector('.mediumseagreen');
+      var ghost = document.querySelectorAll('.mediumseagreen-ghosts');
+      element.removeChild(ghost[0]);
+      element.removeChild(ghost[1]);
 
 
       var darkorange = document.querySelector('.mediumseagreen');
@@ -45,6 +51,8 @@ describe('ステージ3（意図した通りに DOM 要素の構造を変更で�
       elementToAdd.textContent = '\uD83D\uDC2C';
 
       // 上の elementToAdd を追加するコードをここに記述してください。
+      var element = document.querySelector('.turquoise');
+      element.appendChild(elementToAdd);
 
 
       var turquoise = document.querySelector('.turquoise');
@@ -60,6 +68,8 @@ describe('ステージ3（意図した通りに DOM 要素の構造を変更で�
 
       // 上の elementToAdd を、5 番の青色の要素の最初に追加するコードを
       // ここに記述してください。
+      var element = document.querySelector('blockquote');
+      element.insertBefore(elementToAdd,element.firstChild);
 
 
       var blockquote = document.querySelector('blockquote');
@@ -78,7 +88,7 @@ describe('ステージ3（意図した通りに DOM 要素の構造を変更で�
       // jQuery でも同じことをおこなってみましょう。
       // ここに以下のコードを記述してください。
       //
-      // $('.brown-ghost').remove();
+      $('.brown-ghost').remove();
 
 
       var $brown = $('#brown');
@@ -90,6 +100,7 @@ describe('ステージ3（意図した通りに DOM 要素の構造を変更で�
     it('7 番の要素からインベーダー要素を除去する', function() {
 
       // ここにコードを記述してください。
+      $('.darkorange-space-invader').remove();
 
 
       var $darkorange = $('#darkorange');
@@ -104,6 +115,7 @@ describe('ステージ3（意図した通りに DOM 要素の構造を変更で�
     it('8 番の要素の左右の幽霊要素をすべて除去する', function() {
 
       // ここにコードを記述してください。
+      $('.limegreen-ghosts').remove();
 
 
       var $limegreen = $('.limegreen');
@@ -115,6 +127,7 @@ describe('ステージ3（意図した通りに DOM 要素の構造を変更で�
       var $elementToAdd = $('<span>\uD83D\uDC2C</span>');
 
       // 上の $elementToAdd を追加するコードをここに記述してください。
+      $('.mediumturquoise').append($elementToAdd);
 
 
       var $mediumturquoise = $('.mediumturquoise');
@@ -127,6 +140,7 @@ describe('ステージ3（意図した通りに DOM 要素の構造を変更で�
       var $elementToAdd = $('<span>\uD83D\uDC1F</span>');
 
       // 上の $elementToAdd を追加するコードをここに記述してください。
+      $('p').prepend($elementToAdd);
 
 
       var $p = $('p');
